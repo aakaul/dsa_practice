3bbbb0e02b8907fe63b2b473b47145cd45da5d92
abhey <ms.akaul@gmail.com>
2023-07-09 13:13:41 +0530

    2 sums

--- array/medium/2SumProblem.js
+++ array/medium/2SumProblem.js
@@ -1,1 +1,23 @@
 // https://takeuforward.org/data-structure/two-sum-check-if-a-pair-with-given-sum-exists-in-array/
+
+/**
+ * 
+ * @param {number[]} arr 
+ * @param {number} k 
+ * @returns {"Yes"|"No"}
+ */
+function twoSumCheck(arr,k){
+    const totalObj=arr.reduce((acc,cur,i)=>{
+        acc[cur]=i
+        return acc
+    },{});
+    for (const key in totalObj) {
+        if (Object.hasOwnProperty.call(totalObj, key)) {
+            let remainingSum = k-Number(key);
+            if(totalObj[remainingSum]){
+                return"Yes"
+            }
+        }
+    }
+    return "No"
+}
--- array/medium/sortAnArrayof012s.js
+++ array/medium/sortAnArrayof012s.js
@@ -1,1 +1,36 @@
 // https://takeuforward.org/data-structure/sort-an-array-of-0s-1s-and-2s/
+//This problem is a variation of the popular Dutch National flag algorithm.
+/**
+ * 
+ * @param {number[]} arr 
+ * @param {number} k
+ * @returns {number[]} 
+ */
+function sortZerosOnesAndTwos(arr,k){
+    let [low,mid,high]=[0,0,arr.length]
+    while(mid<=high){
+        if(arr[mid]==0){
+            swap(arr,low,mid)
+            mid++;
+            low++;
+        }else if(arr[mid]==1){
+            mid++
+        }else{
+            swap(arr,mid,high)
+            high--;
+        }
+    }
+    return arr
+}
+//swap function
+/**
+ * 
+ * @param {number[]} arr 
+ * @param {number} i 
+ * @param {number} j 
+ */
+function swap(arr,i,j){
+    let temp=arr[i]
+    arr[i]=arr[j]
+    arr[j]=temp
+}
