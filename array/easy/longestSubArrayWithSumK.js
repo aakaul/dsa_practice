0b3a89860810603f338ff9e64301a877ad2dc692
abhey <ms.akaul@gmail.com>
2023-07-09 12:15:15 +0530

    longest negative positive

--- array/easy/longestSubArrayWithSumK.js
+++ array/easy/longestSubArrayWithSumK.js
@@ -1,2 +1,30 @@
 // https://takeuforward.org/arrays/longest-subarray-with-sum-k-postives-and-negatives/
 
+//sliding window approach as in previous snippet will work hence not using it here
+//using logic of hashmap from the videos
+/**
+ * 
+ * @param {number[]} arr 
+ * @param {number} k
+ * @returns {number} 
+ */
+function longestSubArrayWithGivenSumPositiveNegative(arr,k){
+    let map = new Map();
+    let max=0;
+    let sum=0;
+    for(let i=0;i<arr.length;i++){
+        sum+=arr[i];
+        if(sum==k){
+            max=Math.max(max,i+1);
+        }
+        let remainingSum = sum-k;
+        if(map.has(remainingSum)){
+            max = Math.max(max,i-map.get(remainingSum));
+        }
+        //incase zeros are present in the array
+        if(!map.has(sum)){
+            map.set(sum,i);
+        }
+    }
+    return max;
+}
