8f200c0a1399c2305ac290658d036baf039cbadf
abhey kaul <abhey.kaul@pathogenie.in>
2023-07-10 21:10:25 +0530

    2 sums

--- array/medium/kandesAlgo.js
+++ array/medium/kandesAlgo.js
@@ -1,1 +1,39 @@
 // https://takeuforward.org/data-structure/kadanes-algorithm-maximum-subarray-sum-in-an-array/
+
+/**
+ * 
+ * @param {number[]} nums 
+ */
+function kadanesAlgo(nums) {
+    let sum = 0;
+    let max = -Infinity;
+    let [start, ansStart, ansEnd] = [0, -1, -1]
+    for (let i = 0; i < nums.length; i++) {
+
+        if (sum == 0) start = i;
+
+        if (sum == 0) start = i;
+        const el = nums[i];
+        sum += el;
+        if (sum > max) {
+            max = sum;
+
+            ansStart = start;
+            ansEnd = i;
+        }
+        if (sum < 0) {
+            sum = 0
+        }
+    }
+
+
+    //printing the subarray:
+    console.log("The subarray is: [");
+    for (let i = ansStart; i <= ansEnd; i++) {
+        console.log(arr[i] + " ");
+    }
+    console.log("]n");
+
+
+    return max;
+}
--- array/medium/majorityElement.js
+++ array/medium/majorityElement.js
@@ -1,1 +1,68 @@
 // https://takeuforward.org/data-structure/find-the-majority-element-that-occurs-more-than-n-2-times/
+
+/**
+ * 
+ * @param {number[]} nums 
+ */
+// using hashing
+function majorityElement(nums) {
+    const obj = nums.reduce((acc, cur) => {
+        if (!acc[cur]) {
+            acc[cur] = 0
+        }
+        acc[cur]++;
+        return acc
+    }, 0)
+    for (const key in obj) {
+        if (Object.hasOwnProperty.call(obj, key)) {
+            const el = obj[key];
+            if (el > nums.length) {
+                return key
+            }
+        }
+    }
+    return -1
+}
+
+// from video using Moore’s Voting Algorithm:
+/**
+ * 
+ * @param {number[]} nums 
+ */
+function majorityElement2(nums) {
+
+    let sum = 0;
+    let el = null;
+    for (let i = 0; i < nums.length; i++) {
+
+        if (sum == 0) {
+            sum++;
+            el = nums[i]
+        };
+
+        if (el != nums[i]) {
+            sum--;
+        } else {
+            sum++;
+        }
+    }
+
+    /* 
+        need to check this ref https://www.youtube.com/watch?v=n5QY3x_GNDg
+        if(counter > 2){
+            return el
+        }
+        
+    */
+    let counter = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (el == nums[i]) counter++
+    }
+
+    if (counter > nums.length / 2) {
+        return el
+    }
+
+    return -1
+
+}
