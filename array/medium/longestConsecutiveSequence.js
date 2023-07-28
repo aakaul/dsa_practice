4709ab8ab93102b76a5a879c0dfe9f097ac3e97a
abhey kaul <abhey.kaul@pathogenie.in>
2023-07-29 00:53:32 +0530

    longest

--- array/medium/longestConsecutiveSequence.js
+++ array/medium/longestConsecutiveSequence.js
@@ -1,1 +1,31 @@
-// https://takeuforward.org/strivers-a2z-dsa-course/strivers-a2z-dsa-course-sheet-2/#:~:text=Longest%20Consecutive%20Sequence%20in%20an%20%E2%80%A6
+// https://takeuforward.org/data-structure/longest-consecutive-sequence-in-an-array/
+
+/**
+ * 
+ * @param {number[]} params 
+ */
+//[100,4,200,1,3,2]
+function longestConsecutive(params) {
+    const obj = params.reduce((acc, cur) => {
+        acc[cur] = true
+        return acc
+    }, {})
+
+    let maxCount = 0
+    for (let i = 0; i < params.length; i++) {
+        const el = params[i];
+        if (obj[el - 1]) {
+            continue;
+        }
+        let tempEl = el;
+        let tempMaxCount = 0;
+        while (obj[tempEl]) {
+            tempMaxCount++;
+            tempEl++;
+        }
+        maxCount = Math.max(maxCount, tempMaxCount);
+    }
+    return maxCount
+}
+
+longestConsecutive([100, 4, 200, 1, 3, 2])
