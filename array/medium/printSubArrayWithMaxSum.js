b40efc16e5146e5b0ec16844e86464d3d7e621bb
abhey <ms.akaul@gmail.com>
2023-08-08 00:03:08 +0530

    wip

--- array/medium/printSubArrayWithMaxSum.js
+++ array/medium/printSubArrayWithMaxSum.js
@@ -1,1 +1,18 @@
-// https://takeuforward.org/data-structure/kadanes-algorithm-maximum-subarray-sum-in-an-array/
+// https://takeuforward.org/arrays/count-subarray-sum-equals-k/
+
+// https://leetcode.com/problems/subarray-sum-equals-k/
+
+/**
+ *  
+ * @param {number[]} arr 
+ * @param {number} k 
+ */
+function subarraySum(arr,k){
+
+    let map = new Map()
+    let counter =0;
+    let sum = 0;
+    arr.forEach(x=>{
+        map[x]
+    })
+}
